2e60ff274f58494ba5a9101f0367f0dde0740a72
isaacBullinger <isaac.k.bullinger@gmail.com>
2024-03-05 23:36:55 -0500

    Completed Learning05.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Collections.ObjectModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        
+        Console.Clear();
+
+        Square square = new Square("red", 1.5);
+        // Console.WriteLine(square.GetColor());
+        // Console.WriteLine(square.GetArea());
+        // Console.WriteLine();
+
+        Rectangle rectangle = new Rectangle("blue", 2.5, 7.5);
+        // Console.WriteLine(rectangle.GetColor());
+        // Console.WriteLine(rectangle.GetArea());
+        // Console.WriteLine();
+
+        Circle circle = new Circle("yellow", 3.14);
+        // Console.WriteLine(circle.GetColor());
+        // Console.WriteLine(circle.GetArea());
+
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"This {color} shape's area is {area}.");
+        }
     }
 }
---
+++ prepare/Learning05/Square.cs
@@ -0,0 +1,17 @@
+using System.Formats.Asn1;
+
+public class Square : Shape
+{
+    private double _side;
+
+    public Square(string color, double side): base(color)
+    {
+        _side = side;
+    }
+
+    public override double GetArea()
+    {
+        double area = Math.Pow(_side, 2);
+        return area;
+    }
+}
